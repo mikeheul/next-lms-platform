8a37c767444415ad046172632076b0336242a108
Micka <mickael.murmann@gmail.com>
2024-04-08 10:49:25 +0200

    Authentication with clerk works !

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>This is a protected page</p>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   );
 }
