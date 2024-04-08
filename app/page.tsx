a974bdde107c64357ae9f8da17171984841abc51
Micka <mickael.murmann@gmail.com>
2024-04-08 09:20:16 +0200

    Install Button component from shadcn ui (destructive for "danger button")

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-medium text-sky-700">Hello</p>
+    <div>
+      <p className="text-3xl font-medium text-sky-700">Hello</p>
+      <Button variant="destructive">Click me</Button>
+    </div>
   );
 }
