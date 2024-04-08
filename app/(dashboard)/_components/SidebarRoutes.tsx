9fc9803d0fded79662aed380e2f6ead5b2a95b68
Micka <mickael.murmann@gmail.com>
2024-04-08 14:16:13 +0200

    Sidebar dynamically change items if teacher mode

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
 import SidebarItem from "./SidebarItem";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
     {
@@ -15,9 +16,25 @@ const guestRoutes = [
     },
 ]
 
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses"
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics"
+    },
+]
+
 const SidebarRoutes = () => {
 
-    const routes = guestRoutes;
+    const pathname = usePathname();
+    const isTeacherPage = pathname?.includes("/teacher");
+    
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return ( 
         <div className="flex flex-col w-full">
@@ -30,7 +47,7 @@ const SidebarRoutes = () => {
                 />
             ))}
         </div>
-     );
+    );
 }
- 
+
 export default SidebarRoutes;
--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
@@ -9,7 +9,6 @@ import Link from "next/link";
 const NavbarRoutes = () => {
 
     const pathname = usePathname();
-    const router = useRouter();
 
     // teachers has different view
     const isTeacherPage = pathname?.startsWith("/teacher");
@@ -19,10 +18,12 @@ const NavbarRoutes = () => {
     return ( 
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
-                <Button size="sm" variant="ghost">
-                    <LogOut className="h-4 w-4 mr-2" />
-                    Exit
-                </Button>
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
             ) : (
                 <Link href="/teacher/courses">
                     <Button size="sm" variant="ghost">
