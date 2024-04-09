16fd01cd584c3b1c2ba9bae8b0e50bb39e9e5f8c
Micka <mickael.murmann@gmail.com>
2024-04-09 09:45:48 +0200

    Add Textarea component to course setup page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import TitleForm from "./_components/TitleForm";
+import DescriptionForm from "./_components/DescriptionForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
@@ -53,6 +54,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                         <h2 className="text-xl">Customize your course</h2>
                     </div>
                     <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm
                         initialData={course}
                         courseId={course.id}
                     />
