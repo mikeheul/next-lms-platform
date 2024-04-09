e8e6233fab2b287dbef50a714535ae75e6dcd00e
Micka <mickael.murmann@gmail.com>
2024-04-09 18:34:20 +0200

    Courses list test in Courses homepage

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,16 +1,44 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
-    return ( 
-        <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Course
-                </Button>
-            </Link>
-        </div>
-     );
+async function getCourses() {
+
+    const courses = await db.course.findMany({
+        orderBy: {
+            title: 'asc'
+        }
+    })
+
+    return courses;
 }
- 
+
+const CoursesPage = async () => {
+
+    const courses = await getCourses();
+
+    return (
+        <div>
+            <div className="p-6">
+                <Link href="/teacher/create">
+                    <Button>
+                        New Course
+                    </Button>
+                </Link>
+            </div>
+            <div className="grid grid-cols-3 gap-x-2 gap-y-4 p-6">
+                {courses.map((course) => (
+                    <Link
+                        className="flex flex-col p-6 border border-slate-200 rounded-md"
+                        key={course.id}
+                        href={`/teacher/courses/${course.id}`}
+                    >
+                        { course.title }
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
+
 export default CoursesPage;
