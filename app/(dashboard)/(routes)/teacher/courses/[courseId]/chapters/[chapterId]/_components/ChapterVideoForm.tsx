7f543602870bea3cef0ecca6ce9d42f7a6ec3228
Micka <mickael.murmann@gmail.com>
2024-04-15 15:27:31 +0200

    Create ChapterVideoForm component

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterVideoForm.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+// Importing Zod library for schema validation
+import * as z from "zod";
+// Importing Axios for making HTTP requests
+import axios from "axios";
+// Importing Button component for form buttons
+import { Button } from "@/components/ui/button";
+// Importing Pencil icon from lucide-react
+import { Pencil, PlusCircle, Video } from "lucide-react";
+// Importing useState hook for managing component state
+import { useState } from "react";
+// Importing toast for displaying notifications
+import toast from "react-hot-toast";
+// Importing useRouter hook from next/navigation for navigating within the application
+import { useRouter } from "next/navigation";
+import { Chapter, MuxData } from "@prisma/client";
+import { FileUpload } from "@/components/FileUpload";
+
+import Image from "next/image";
+
+interface ChapterVideoFormProps {
+    initialData: Chapter & { muxData?: MuxData | null };
+    courseId: string;
+    chapterId: string;
+}
+
+// Defining form schema using Zod
+const formSchema = z.object({
+    videoUrl : z.string().min(1),
+});
+
+const ChapterVideoForm = ({
+    initialData,
+    courseId,
+    chapterId
+}: ChapterVideoFormProps) => {
+
+    // State variable for controlling editing mode
+    const [isEditing, setIsEditing] = useState(false);
+
+    // Function to toggle editing mode
+    const toggleEdit = () => setIsEditing((current) => !current);
+
+    // useRouter hook for navigation
+    const router = useRouter();
+
+    // Function to handle form submission
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            // Sending PATCH request to update course
+            await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+            // Displaying success notification
+            toast.success("Chapter updated !");
+            // Exiting edit mode
+            toggleEdit();
+            // Refreshing page
+            router.refresh();
+        } catch {
+            // Displaying error notification
+            toast.error("Something went wrong");
+        }
+    };
+
+    // Rendering JSX for DescriptionForm component
+    return ( 
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course image
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing && (
+                        <>Cancel</>
+                    )}
+
+                    {!isEditing && !initialData.videoUrl && (
+                        <>
+                            <PlusCircle className="h-4 w-4 mr-2" />
+                            Add a video
+                        </>
+                    )}
+                    
+                    {!isEditing && initialData.videoUrl && (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2" />
+                            Edit video
+                        </>
+                    )}
+                </Button>
+            </div>
+            {!isEditing && (
+                !initialData.videoUrl ? (
+                    <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+                        <Video className="h-10 w-10 text-slate-500" />
+                    </div>
+                ) : (
+                    <div className="relative aspect-video mt-2">
+                        video uploaded!
+                    </div>
+                )
+            )}
+            {isEditing && (
+                <div>
+                    <FileUpload 
+                        endpoint="courseImage"
+                        onChange={(url) => {
+                            if(url) {
+                                onSubmit({ videoUrl: url })
+                            }
+                        }}
+                    />
+                    <div className="text-xs text-muted-foreground mt-4">
+                        Upload this chapter&apos;s video
+                    </div>
+                </div>
+            )}
+            {initialData.videoUrl && !isEditing && (
+                <div>
+                    Videos can take a few minutes to process. Refresh the page if video does not appear.
+                </div>
+            )}
+        </div>
+    );
+}
+
+// Export the DescriptionForm component as the default export
+export default ChapterVideoForm;
