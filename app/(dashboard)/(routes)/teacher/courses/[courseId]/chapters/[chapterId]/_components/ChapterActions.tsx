835e1843b12b5f93cd8571d111abf9d597a8b76a
Micka <mickael.murmann@gmail.com>
2024-04-16 10:21:14 +0200

    Delete a chapter works !

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -2,7 +2,11 @@
 
 import ConfirmModal from "@/components/modals/ConfirmModal";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ChapterActionsProps {
     disabled: boolean;
@@ -17,18 +21,39 @@ const ChapterActions = ({
     chapterId,
     isPublished
 }: ChapterActionsProps) => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+
+    // delete a chapter
+    const onDelete = async () => {
+        try {
+            setIsLoading(true);
+
+            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+            toast.success("Chapter deleted successfully");
+            router.refresh();
+            router.push(`/teacher/courses/${courseId}`)
+
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return ( 
         <div className="flex items-center gap-x-2">
             <Button
                 onClick={() => {}}
-                disabled={disabled}
+                disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
             >
                 {isPublished ? "Unpublish" : "Publish"}
             </Button>
-            <ConfirmModal onConfirm={() => {}}>
-                <Button size="sm">
+            <ConfirmModal onConfirm={onDelete}>
+                <Button size="sm" disabled={isLoading}>
                     <Trash className="h-4 w-4" />
                 </Button>
             </ConfirmModal>
