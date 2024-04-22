4da47c970a01ae83a1718ddcc776ed0ea58fef35
Micka <mickael.murmann@gmail.com>
2024-04-22 11:02:21 +0200

    Add edit button in datatable

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -3,7 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
+import { 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link"
 
 export const columns: ColumnDef<Course>[] = [
     {
@@ -48,4 +55,29 @@ export const columns: ColumnDef<Course>[] = [
             )
         },
     },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const { id } = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-4 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <Link href={`/teacher/courses/${id}`}>
+                            <DropdownMenuItem>
+                                <Pencil className="h-4 w-4 mr-2"/>
+                                Edit
+                            </DropdownMenuItem>
+                        </Link>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
+    }
 ]
