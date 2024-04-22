b69906818bb7214513d243fb6693458e9fd43e36
Micka <mickael.murmann@gmail.com>
2024-04-22 14:31:35 +0200

    Change position of SearchInput mobile / desktop view

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import Categories from "./_components/Categories";
+import SearchInput from "@/components/SearchInput";
 
 const SearchPage = async () => {
 
@@ -10,12 +11,17 @@ const SearchPage = async () => {
     });
 
     return ( 
-        <div className="p-6">
-            <Categories 
-                items={categories}
-            />
-        </div>
-     );
+        <>
+            <div className="px-6 pt-6 md:hidden md:mb-0 block">
+                <SearchInput />
+            </div>
+            <div className="p-6">
+                <Categories 
+                    items={categories}
+                />
+            </div>
+        </>
+    );
 }
- 
+
 export default SearchPage;
