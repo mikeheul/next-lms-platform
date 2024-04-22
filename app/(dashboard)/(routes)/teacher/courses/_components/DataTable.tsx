7b0790e7d2ffd438bbff00f7fe7d6349099be17c
Micka <mickael.murmann@gmail.com>
2024-04-22 10:42:16 +0200

    Setup pagination for datatable

--- app/(dashboard)/(routes)/teacher/courses/_components/DataTable.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/DataTable.tsx
@@ -4,9 +4,11 @@ import * as React from "react"
 
 import {
     ColumnDef,
+    ColumnFiltersState,
     SortingState,
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     getPaginationRowModel,
     getSortedRowModel,
     useReactTable,
@@ -20,7 +22,9 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -33,6 +37,9 @@ export function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
 
     const [sorting, setSorting] = React.useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+        []
+    )
 
     const table = useReactTable({
         data,
@@ -41,13 +48,31 @@ export function DataTable<TData, TValue>({
         getPaginationRowModel: getPaginationRowModel(),
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
         state: {
             sorting,
+            columnFilters,
         },
+        initialState: {
+            pagination: {
+                pageSize: 5,
+            },
+        }
     })
 
     return (
         <div>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter course by title..."
+                    value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("title")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
             <div className="rounded-md border">
                 <Table>
                     <TableHeader>
