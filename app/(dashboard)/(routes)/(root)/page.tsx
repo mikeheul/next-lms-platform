a79dc97d90794fdb803747387c66859d9e30df70
Micka <mickael.murmann@gmail.com>
2024-04-23 13:53:53 +0200

    Dashboard displays courses (first in progress, then completed)

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,9 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import CoursesList from "@/components/CoursesList";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+
+  const { userId } = auth();
+
+  if(!userId) return redirect("/")
+
+  const {
+    completedCourses,
+    coursesInProgress
+  } = await getDashboardCourses(userId)
+
   return (
-    <div>
-      
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div>
+          {/* info card */}
+        </div>
+        <div>
+          {/* info card */}
+        </div>
+      </div>
+      <CoursesList 
+        items={[...coursesInProgress, ...completedCourses]}
+      />
     </div>
   );
 }
